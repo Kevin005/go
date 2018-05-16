9d2b55b9202eb4090b4e5c719815b895911f4204
liuwei01 <liuwei01@zenmen.com>
2018-05-16 18:28:08 +0800

    increate webDemo

---
+++ src/awesomeProject/web/webDemo/handlers.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+	"log"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
+	log.Println(r.URL, r.Method)
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	todos := Todos{
+		Todo{Name: "Write presentation"},
+		Todo{Name: "Host meetup"},
+	}
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
+		panic(err)
+	}
+	log.Println(r.URL, r.Method)
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintln(w, "Todo show:", todoId)
+	log.Println(r.URL, r.Method)
+}
